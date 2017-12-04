40bf9a2e1342c3f52c941ffbc57d26112be0dc42
Tyge <tygesjohnso@gmail.com>
2017-12-04 11:54:56 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express();
+
+server.get('/', (req, res) => {
+  res.send({hello: 'world'});
+})
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server up on port ${PORT}`);
+});
