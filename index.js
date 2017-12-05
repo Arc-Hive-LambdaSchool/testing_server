ef42bb42429acb64191a2fd23278d75b0cf071b0
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-12-04 19:46:38 -0500

    connected to mlab

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
 const server = express();
+// const mongoose = require('mongoose');
+//mongoose.connect('mongodb://arc_hive_admin:arc hive 555@ds013475.mlab.com:13475/arc_hive_testdb', {useMongoClient: true});
+//mongodb://<dbuser>:<dbpassword>@ds013475.mlab.com:13475/arc_hive_testdb
+const PORT = process.env.PORT || 3000;
+const MONGO_URL = 'mongodb://arc_hive_admin:arc hive 555@ds013475.mlab.com:13475/arc_hive_testdb';
+
+MongoClient.connect(MONGO_URL, (err, db) => {  
+  if (err) {
+    return console.log(err);
+  }
+
+  // Do something with db here, like inserting a record
+  db.collection('notes').insertOne(
+    {
+      title: 'Hello MongoDB',
+      text: 'Hopefully this works!'
+    },
+    function (err, res) {
+      if (err) {
+        db.close();
+        return console.log(err);
+      }
+      // Success
+      db.close();
+    }
+  )
+});
 
 server.get('/', (req, res) => {
   res.send({hello: 'world'});
 })
 
-const PORT = process.env.PORT || 3000;
-
 server.listen(PORT, () => {
   console.log(`Server up on port ${PORT}`);
 });
-// did this work?
