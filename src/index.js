15373c2bedc90f21f563366d4e2af56e271b398e
Tyge <tygesjohnso@gmail.com>
2017-12-05 11:51:15 -0700

    heroku fixed

--- src/index.js
+++ src/index.js
@@ -33,7 +33,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.send('<h2>The Slash Command and Dialog app is running</h2> <p>Follow the' +
+  res.send('<h2>TEST The Slash Command and Dialog app is running</h2> <p>Follow the' +
   ' instructions in the README to configure the Slack App and your environment variables.</p>');
 });
 
