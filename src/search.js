d122589b8ba9e11238797d100360dbb8fd03c2d7
Tyge <tygesjohnson@gmail.com>
2017-12-06 15:09:05 -0700

    test

--- src/search.js
+++ src/search.js
@@ -8,34 +8,34 @@ const users = require('./users');
  *  chat.postMessage to the user who created it
  */
 const sendConfirmation = (search) => {
+  console.log(search);
   axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
     token: process.env.SLACK_ACCESS_TOKEN,
     channel: search.userId,
     text: 'View links below',
-    attachments: JSON.stringify (search),
-    // attachments: JSON.stringify([
-    //   {
-    //     title: `Ticket created for ${search.userEmail}`,
-    //     // Get this from the 3rd party helpdesk system
-    //     title_link: 'http://example.com',
-    //     text: search.text,
-    //     fields: [
-    //       // {
-    //       //   title: 'Title',
-    //       //   // value: search.title,
-    //       // },
-    //       {
-    //         title: 'Tags',
-    //         value: search.tags || 'None provided',
-    //       },
-    //       {
-    //         title: 'Cohort',
-    //         value: search.cohort || 'None provided',
-    //         short: true,
-    //       },
-    //     ],
-    //   },
-    // ]),
+    attachments: JSON.stringify([
+      {
+        title: `Ticket created for ${search.userEmail}`,
+        // Get this from the 3rd party helpdesk system
+        title_link: 'http://example.com',
+        text: search.text,
+        fields: [
+          // {
+          //   title: 'Title',
+          //   // value: search.title,
+          // },
+          {
+            title: 'Tags',
+            value: search.tags || 'None provided',
+          },
+          {
+            title: 'Cohort',
+            value: search.cohort || 'None provided',
+            short: true,
+          },
+        ],
+      },
+    ]),
   })).then((result) => {
     debug('sendConfirmation: %o', result.data);
   }).catch((err) => {
