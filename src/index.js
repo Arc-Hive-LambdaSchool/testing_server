1305e094007b5a36b31fbb43fd0b474c52fb6756
Tyge <tygesjohnson@gmail.com>
2017-12-06 12:37:34 -0700

    bug fixing

--- src/index.js
+++ src/index.js
@@ -53,7 +53,7 @@ app.post('/commands', (req, res) => {
       token: process.env.SLACK_ACCESS_TOKEN,
       trigger_id,
       dialog: JSON.stringify({
-        title: 'Sumbit a helpdesk ticket',
+        title: 'hello',
         callback_id: 'submit-ticket',
         submit_label: 'Submit',
         elements: [
