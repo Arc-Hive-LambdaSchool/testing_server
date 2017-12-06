4e468819eccf3f9f3182368bf660b762a8ace568
Tyge <tygesjohnson@gmail.com>
2017-12-06 15:55:14 -0700

    index.js edit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,3 @@
-// const express = require('express');
-// const server = express();
-// const request = require('request')
-// const bodyParser = require('body-parser')
-// const urlencodedParser = bodyParser.urlencoded({ extended: false })
-//
-// server.get('/', (req, res) => {
-//   res.send({hello: 'world'});
-// })
-//
-// const PORT = process.env.PORT || 3000;
-//
-// server.listen(PORT, () => {
-//   console.log(`Server up on port ${PORT}`);
-// });
-
 require('dotenv').config();
 
 const axios = require('axios');
