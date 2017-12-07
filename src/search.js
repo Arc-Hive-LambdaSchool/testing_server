6dd1cb66fdc722fe10b43e3802f9a5bf93fa8df8
Tyge <tygesjohnson@gmail.com>
2017-12-07 12:24:04 -0700

    arc testing

--- src/search.js
+++ src/search.js
@@ -45,6 +45,44 @@ const sendConfirmation = (slackSearch) => {
   });
 };
 
+const arcConfirmation = (slackSearch) => {
+  console.log(slackSearch);
+  axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
+    token: process.env.SLACK_ACCESS_TOKEN,
+    channel: slackSearch.userId,
+    text: 'View links below',
+    attachments: JSON.stringify([
+      {
+        title: `Ticket created for ${slackSearch.userEmail}`,
+        // Get this from the 3rd party helpdesk system
+        title_link: 'http://example.com',
+        text: slackSearch.text,
+        fields: [
+          {
+            title: 'Tags',
+            value: slackSearch.tags || 'None provided',
+          },
+          {
+            title: 'Cohort',
+            value: slackSearch.cohort || 'None provided',
+            short: true,
+          },
+          {
+            title: 'Brownbag',
+            value: slackSearch.brownbag || 'No',
+          }
+        ],
+      },
+    ]),
+  })).then((result) => {
+    debug('arcConfirmation: %o', result.data);
+  }).catch((err) => {
+    debug('arcConfirmation error: %o', err);
+    console.error(err);
+  });
+};
+
+
 // Create helpdesk slackSearch. Call users.find to get the user's email address
 // from their user ID
 const create = (userId, submission) => {
@@ -64,7 +102,7 @@ const create = (userId, submission) => {
     slackSearch.tags = submission.tags;
     slackSearch.cohort = submission.cohort;
     slackSearch.brownbag = submission.brownbag;
-    sendConfirmation(slackSearch);
+    arcConfirmation(slackSearch);
     return slackSearch;
   }).catch((err) => { console.error(err); });
 };
