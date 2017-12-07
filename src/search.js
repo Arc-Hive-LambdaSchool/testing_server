d7a7ee985c5b15b6c220c3305a917dfd2dfa02c3
Tyge <tygesjohnson@gmail.com>
2017-12-07 15:30:49 -0700

    testing

--- src/search.js
+++ src/search.js
@@ -51,7 +51,7 @@ const arcConfirmation = (slackSearch) => {
     token: process.env.SLACK_ACCESS_TOKEN,
     // response_type: "in_channel",
     channel: `#${slackSearch.cohort}`,
-    text: '@channel Video is now uploaded',
+    text: '@channel',
     attachments: JSON.stringify([
       {
         title: `Ticket created for ${slackSearch.userEmail}`,
