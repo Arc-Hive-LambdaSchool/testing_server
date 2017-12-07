890065d1251788e945712fc8a208307b62ddd5bf
Tyge <tygesjohnson@gmail.com>
2017-12-06 18:04:03 -0700

    testing

--- src/index.js
+++ src/index.js
@@ -102,6 +102,84 @@ app.post('/commands', (req, res) => {
   }
 });
 
+app.post('/arcCommands', (req, res) => {
+  // extract the verification token, slash command text,
+  // and trigger ID from payload
+  const { token, text, trigger_id } = req.body;
+
+  // check that the verification token matches expected value
+  if (token === process.env.SLACK_VERIFICATION_TOKEN) {
+    // create the dialog payload - includes the dialog structure, Slack API token,
+    // and trigger ID
+    const dialog = {
+      token: process.env.SLACK_ACCESS_TOKEN,
+      trigger_id,
+      dialog: JSON.stringify({
+        title: 'LS Videos',
+        callback_id: 'submit-search',
+        submit_label: 'Submit',
+        elements: [
+          // {
+          //   label: 'Title',
+          //   // type: 'text',
+          //   // name: 'title',
+          //   value: 'https://pacific-waters-60975.herokuapp.com/',
+          //   // hint: '30 second summary of the problem',
+          // },
+          {
+            label: 'Tags',
+            type: 'select',
+            name: 'tags',
+            optional: true,
+            options: [
+              { label: 'JS', value: 'JS' },
+              { label: 'React', value: 'React' },
+              { label: 'Redux', value: 'Redux' },
+              { label: 'Auth', value: 'Auth' },
+              { label: 'C', value: 'C' },
+              { label: 'Testing', value: 'Testing' },
+            ],
+          },
+          {
+            label: 'Cohort',
+            optional: true,
+            type: 'select',
+            name: 'cohort',
+            options: [
+              { label: 'CS1', value: 'CS1' },
+              { label: 'CS2', value: 'CS2' },
+              { label: 'CS3', value: 'CS3' },
+              { label: 'CS4', value: 'CS4' },
+            ],
+          },
+          {
+            label: 'Brownbag?',
+            optional: true,
+            type: 'select',
+            name: 'brownbag',
+            options: [
+              { label: 'Yes', value: 'true' },
+            ]
+          }
+        ],
+      }),
+    };
+
+    // open the dialog by calling dialogs.open method and sending the payload
+    axios.post('https://slack.com/api/dialog.open', qs.stringify(dialog))
+      .then((result) => {
+        debug('dialog.open: %o', result.data);
+        res.send('');
+      }).catch((err) => {
+        debug('dialog.open call failed: %o', err);
+        res.sendStatus(500);
+      });
+  } else {
+    debug('Verification token mismatch');
+    res.sendStatus(500);
+  }
+});
+
 /*
  * Endpoint to receive the dialog submission. Checks the verification token
  * and creates a Helpdesk ticket
