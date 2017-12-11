27f5319af967466177ab9340bbd35973b16c32bb
Tyge <tygesjohnson@gmail.com>
2017-12-11 12:28:04 -0700

    test

--- src/index.js
+++ src/index.js
@@ -62,18 +62,18 @@ app.post('/commands', (req, res) => {
               { label: 'Testing', value: 'Testing' },
             ],
           },
-          // {
-          //   label: 'Cohort',
-          //   optional: true,
-          //   type: 'select',
-          //   name: 'cohort',
-          //   options: [
-          //     { label: 'CS1', value: 'CS1' },
-          //     { label: 'CS2', value: 'CS2' },
-          //     { label: 'CS3', value: 'CS3' },
-          //     { label: 'CS4', value: 'CS4' },
-          //   ],
-          // },
+          {
+            label: 'Cohort',
+            optional: true,
+            type: 'select',
+            name: 'cohort',
+            options: [
+              { label: 'CS1', value: 'CS1' },
+              { label: 'CS2', value: 'CS2' },
+              { label: 'CS3', value: 'CS3' },
+              { label: 'CS4', value: 'CS4' },
+            ],
+          },
           {
             label: 'Brownbag?',
             optional: true,
