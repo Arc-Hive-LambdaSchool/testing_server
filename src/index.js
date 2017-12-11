8eb0e88082d2248c8788adc8bc9eb24ecb8f1f8d
Tyge <tygesjohnson@gmail.com>
2017-12-11 12:24:03 -0700

    test

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('<h2>The Slash Command and Dialog app is running</h2> <p>Follow the' +
-  ' instructions in the README to configure the Slack App and your environment variables.</p>');
+  ' instructions in the README to configure the Slack App and your environment variables!</p>');
 });
 
 /*
