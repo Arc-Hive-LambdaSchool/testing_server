ada6cc5c3f320c9c3655454c35841cbd5da9e72e
Tyge <tygesjohnson@gmail.com>
2017-12-11 12:37:43 -0700

    test

--- src/search.js
+++ src/search.js
@@ -49,7 +49,7 @@ const arcConfirmation = (slackSearch) => {
   console.log(slackSearch);
   axios.post('https://slack.com/api/chat.postMessage', qs.stringify({
     token: process.env.SLACK_ACCESS_TOKEN,
-    // response_type: "in_channel",
+    response_type: "in_channel",
     channel: `#${slackSearch.cohort}`,
     text: '@channel',
     attachments: JSON.stringify([
